6f0b02737d5b40cd098623fae8a3670c33cf68ee
camillesosa <71521586+camillesosa@users.noreply.github.com>
2023-11-23 17:24:11 -0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,18 +1,6 @@
 <?php
-    //if not logged in (after successfully logging out), redirect to login page
-    if (!isset($_SESSION['username'])) {
-        header("Location: login.html");
-        exit();
-    }
-
-    unset($_SESSION['username']);
-    $_SESSION['username'] = '';
-    // Retrieve the existing value of the cookie
-    $existingValue = $_COOKIE['PHPSESSID'];
-
-    // Set the cookie expiration time to a past date
-    setcookie("PHPSESSID", "", time() - 3600);
-
-    // Display a message to confirm the update
-    echo "Cookie updated successfully.";
+        // Display a message to confirm the update
+    echo "<script>console.log($session)</script>";
+    setcookie('PHPSESSID', '', -1, '/');
+    header("Location: login.html");
 ?>
