9686a4afa1953261801feb42720c776cce38ee28
uhhrace <racebays2@gmail.com>
2023-12-13 00:35:51 -0800

    #RB Compose app with multiple containers, multiple dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,8 @@ COPY . .
 # Install any necessary dependencies
 RUN apt-get update && apt-get install -y \
     libpq-dev \
-    && docker-php-ext-install mysqli
+    && docker-php-ext-install mysqli \ 
+    && docker-php-ext-enable mysqli
 
 # Expose port 80 for Apache
 EXPOSE 80
--- util/config.php
+++ util/config.php
@@ -1,7 +1,8 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
+// TODO change to docker container name
+define('DB_SERVER', 'my-game-tracker-mygamedb-1');
 define('DB_USERNAME', 'greenteam');
 define('DB_PASSWORD', 'ra8t9kw8');
 define('DB_NAME', 'greenteam');
@@ -11,6 +12,7 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
 if($link === false){
+    echo "Failed with params: " . DB_SERVER . " " . DB_USERNAME . " " . DB_PASSWORD . " " . DB_NAME . " 3306";
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
